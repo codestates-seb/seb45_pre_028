aa9b1fdf5ccf5f23dcbe625a4b4447f26350199b
pearl-sea <sakanaplanet@gmail.com>
2023-08-10 13:37:58 +0900

    feat: common css 상수 추가

---
+++ client/src/constants/common_css.ts
@@ -0,0 +1,12 @@
+interface CommonCss {
+  [key: string]: string;
+}
+
+export const COMMON_CSS: CommonCss = {
+  "main-color": "#f48225",
+  "border-color": "#d6d9dc",
+  "font-color-black": "#3b4045",
+  "font-color-gray": "#525960",
+  "button-color": "#0a95ff",
+  "button-hover-color": "#0074cc",
+};
