e88cf7492b68f7f43d2c9a825c3b67a7ac13b404
uudean <swj4545@naver.com>
2023-08-18 00:02:33 +0900

    feat: OAuth2 로그인 구현

---
+++ server/src/main/java/com/preproject/server/auth/handler/OAuth2MemberSuccessHandler.java
@@ -0,0 +1,82 @@
+package com.preproject.server.auth.handler;
+
+import com.preproject.server.auth.jwt.JwtTokenizer;
+import com.preproject.server.auth.utils.CustomAuthorityUtils;
+import com.preproject.server.member.entity.Member;
+import com.preproject.server.member.repository.MemberRepository;
+import com.preproject.server.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+    private final JwtTokenizer jwtTokenizer;
+    private final CustomAuthorityUtils customAuthorityUtils;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+                                        HttpServletResponse response,
+                                        Authentication authentication) throws IOException, ServletException {
+        var oAuth2User = (OAuth2User) authentication.getPrincipal();
+        String email = String.valueOf(oAuth2User.getAttributes().get("email"));
+        String name = String.valueOf(oAuth2User.getAttributes().get("name"));
+
+        List<String> authorities = customAuthorityUtils.createRoles(email);
+
+        saveMember(email, name);
+        redirect(request, response, email, authorities);
+    }
+
+    private void saveMember(String email, String name) {
+        Member member = new Member();
+        member.setEmail(email);
+        member.setName(name);
+
+        memberRepository.save(member);
+    }
+
+    private void redirect(HttpServletRequest request,
+                          HttpServletResponse response,
+                          String username,
+                          List<String> authorities) throws IOException {
+        String accessToken = delegateAccessToken(username, authorities);
+        String refreshToken = delegateRefreshToken(username);
+
+    }
+
+    private String delegateAccessToken(String email, List<String> authorities) {
+
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("email", email);
+        claims.put("roles", authorities);
+
+        String subject = email;
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getAccessTokenExpirationMinutes());
+        String base64EncodedSecretKey = jwtTokenizer.encodedBase64SecretKey(jwtTokenizer.getSecretKey());
+        String accessToken = jwtTokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return accessToken;
+    }
+
+    private String delegateRefreshToken(String email) {
+
+        String subject = email;
+        Date expiration = jwtTokenizer.getTokenExpiration(jwtTokenizer.getRefreshTokenExpirationMinutes());
+        String base64EncodedSecretKey = jwtTokenizer.encodedBase64SecretKey(jwtTokenizer.getSecretKey());
+        String refreshToken = jwtTokenizer.generateRefreshToken(subject, expiration, base64EncodedSecretKey);
+
+        return refreshToken;
+    }
+}
