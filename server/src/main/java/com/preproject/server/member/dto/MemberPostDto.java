9c6380a36d07df4cf0d3f28c68f5a460208616a9
uudean <swj4545@naver.com>
2023-08-18 00:01:28 +0900

    feat: 코드 수정

--- server/src/main/java/com/preproject/server/member/dto/MemberPostDto.java
+++ server/src/main/java/com/preproject/server/member/dto/MemberPostDto.java
@@ -19,7 +19,7 @@ public class MemberPostDto {
     @Size(min = 2,max = 12,message = "이름은 2자 이상 12자 이하여야 합니다.")
     private String name;
 
-    @NotBlank(message = "비밀번호는 공배이 아니어야 합니다.")
+    @NotBlank(message = "비밀번호는 공백이 아니어야 합니다.")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,12}$",
     message = "비밀번호는 8자리 이상 12자리 이하 영문자와 숫자 특수문자를 포함하여야 합니다.")
     private String password;
