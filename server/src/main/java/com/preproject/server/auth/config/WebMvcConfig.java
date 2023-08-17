a10be9fe54328e8c993cccfce9fe481860f0b4e9
uudean <swj4545@naver.com>
2023-08-18 00:02:19 +0900

    feat: CORS config 구현

---
+++ server/src/main/java/com/preproject/server/auth/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package com.preproject.server.auth.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
