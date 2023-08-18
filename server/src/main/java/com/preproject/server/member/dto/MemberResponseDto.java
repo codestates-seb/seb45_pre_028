acb400374c6a0f53152066f1b43da494ade5ead1
bonbon0808 <130390467+bonbon0808@users.noreply.github.com>
2023-08-18 11:21:12 +0900

    Update MemberResponseDto.java

--- server/src/main/java/com/preproject/server/member/dto/MemberResponseDto.java
+++ server/src/main/java/com/preproject/server/member/dto/MemberResponseDto.java
@@ -11,14 +11,7 @@ public class MemberResponseDto {
     private long memberId;
     private String email;
     private String name;
-
-    /**
-     *  questionCount, answerCount 필드 추가 @Setter 추가
-     */
     private long questionCount;
     private long answerCount;
 
-
-    //    // 바디에 비밀번호가 포함될 필요는 없다고 해서 일단 주석처리!
-//    private String password;
 }
