7c4154b4fb06e183269c7e71b757a213617a2a1e
uudean <swj4545@naver.com>
2023-08-21 15:40:34 +0900

    feat: 로그인 response 추가

---
+++ server/src/main/java/com/preproject/server/auth/login/dto/LoginResponseDto.java
@@ -0,0 +1,16 @@
+package com.preproject.server.auth.login.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginResponseDto {
+    private long memberId;
+    private String accessToken;
+
+    public LoginResponseDto(long memberId,String accessToken) {
+        this.memberId = memberId;
+        this.accessToken=accessToken;
+    }
+}
