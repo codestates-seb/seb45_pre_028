a5f5893cec9549e5bcd5bd44a44348db40b1b41a
pearlsea <sakanaplanet@gmail.com>
2023-08-21 15:12:44 +0900

    feat: 커스텀 훅 토큰 처리 로직 추가

--- client/src/hooks/useFetch.tsx
+++ client/src/hooks/useFetch.tsx
@@ -9,16 +9,29 @@ export interface FetchReturn<T> {
   data: T;
 }
 
-export const useFetch = <T extends object>(atom: RecoilState<T>, url: string): FetchReturn<T> => {
+export const useFetch = <T extends object>(
+  atom: RecoilState<T>,
+  url: string,
+  token: string | null = null,
+): FetchReturn<T> => {
   const [isLoading, setIsLoading] = useState(false);
   const [isError, setIsError] = useState(false);
   const [data, setData] = useRecoilState(atom);
+
+  type Headers = Record<string, string>;
+  const headers: Headers = {
+    "ngrok-skip-browser-warning": "69420",
+  };
+
+  if (token) {
+    headers["Authorization"] = `Bearer ${token}`;
+  }
+
   const fetchData = async () => {
     setIsLoading(true);
     setIsError(false);
-
     try {
-      const response = await axios.get(url);
+      const response = await axios.get(url, { headers });
       setData(response.data);
       setIsLoading(false);
     } catch (error) {
