2cc6a17d7a044aac9839f8c5014077d5deb2ee51
pearlsea <sakanaplanet@gmail.com>
2023-08-22 12:06:04 +0900

    fix: atom 초기값 수정

--- client/src/atoms/atoms.ts
+++ client/src/atoms/atoms.ts
@@ -5,7 +5,7 @@ export const questionsState = atom<Question>({
   key: "questionsState",
   default: {
     questionData: [],
-    pageInfo: { page: 1, size: 10, totalElements: 0, totalPages: 0 },
+    pageInfo: { page: 1, size: 10, totalElements: 0, totalPages: 1 },
   },
 });
 
@@ -13,7 +13,7 @@ export const answerState = atom<Answer>({
   key: "answerState",
   default: {
     answerData: [],
-    pageInfo: { page: 1, size: 10, totalElements: 0, totalPages: 0 },
+    pageInfo: { page: 1, size: 10, totalElements: 0, totalPages: 1 },
   },
 });
 
