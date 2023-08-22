d284144cb0181f978e15700759f1243efb091d77
pearlsea <sakanaplanet@gmail.com>
2023-08-22 16:37:14 +0900

    feat: 누락된 import 추가

--- client/src/components/answer/AnswerInput.tsx
+++ client/src/components/answer/AnswerInput.tsx
@@ -1,3 +1,6 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
 import { styled } from "styled-components";
 import { COMMON_CSS } from "../../constants/common_css";
 import { getAccessToken } from "../../util/auth";
